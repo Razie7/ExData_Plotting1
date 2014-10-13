88e4fd1b3626581f2db8e8be1ac9f5e1d2429876
Razie7 <mr_solidsnake87@hotmail.com>
2014-10-13 16:06:16 -0400

    Update Plot2.r

--- Plot2.r
+++ Plot2.r
@@ -2,12 +2,13 @@
 
 
 # Plot 2
+# Make sure that you uploaded the database first by running the 1Upload DB.r file first
 
 with (file, plot(Datetime1,Global_active_power, type="l",
          
                  xlab="", 
                  ylab="Global active power(Kilowatts)"))
 # save the file in PNG format
-dev.copy(png,file="plot22.png",width = 480, height = 480, 
+dev.copy(png,file="plot2.png",width = 480, height = 480, 
          units = "px", bg = "transparent")
 dev.off()
